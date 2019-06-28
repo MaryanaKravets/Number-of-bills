8bfff3c76b1fe0d9c57a2eb38eb37a12c206445e
Maryana Kravets <marjana_s@ukr.net>
2019-06-29 00:55:57 +0300

    Added ListIterator

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -3,19 +3,23 @@ package main.java;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.ListIterator;
 
 class Account {
+    void money(Integer number) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 50, 200, 1000, 20, 5, 2, 10, 100, 500));
+        Collections.sort(list, Collections.reverseOrder());
+        ListIterator<Integer> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            Integer a = listIterator.next();
+            int b = number / a;
+            if (b != 0) {
+                System.out.print(b + "*" + a + "; ");
+                number %= a;
+                if (number == 0) break;
+            }
 
-    void money(Integer number){
-    ArrayList<Integer> list= new ArrayList<>(Arrays.asList(1,50,200,1000,20,5,2,10,100,500));
-    Collections.sort(list,Collections.reverseOrder());
-    for (int i=0; i<list.size(); i++){
-        if(number/list.get(i)!=0){
-            System.out.print(number/list.get(i)+"*"+list.get(i)+"; ");
-            number%=list.get(i);
-            if (number==0)break;
         }
-    }
 
-}
+    }
 }
